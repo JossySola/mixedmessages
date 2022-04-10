5538fd5a9366426b925c4ba4b1615fa2903b2f1d
Jossy Solá <jossysola@gmail.com>
2022-04-09 21:13:56 -0500

    Adds .continue and .action words

--- script.js
+++ script.js
@@ -36,14 +36,16 @@ const space = {
 const words = {
     starters: ['The', 'A'],
     verbs: {
-        passive: [' refuel', ' passes', ' approaches', ' relocates', ' goes', ' vibrates', ' hides', ' flies'],
+        passive: [' refuel', ' passes', ' approaches', ' relocates', ' goes', ' vibrates', ' hides', ' flies', ' travels'],
+        continuous: [' passing', ' approaching', ' going', ' vibrating', ' collapsing', ' emerging', ' flying', ' travelling'],
+        actions: [' collides', ' smashes', ' affects', ' crashes'],
         causation: [' causing', ' making', ' generating', ' provoking', ' letting'],
         attack: [' fires', ' sends', ' attacks with', ' directs', ' targets']
     },
-    location: [' in the', ' at the', ' near the', ' right into the', ' below the', ' inside the', ' by'],
-    quantity: [' some', ' many', ' few', ' handful'],
-    weapons: [' missile', ' rocket', ' spicebomb', ' bunch of code', ' virus in an e-mail', ' bug', ' #FatalError', ' lot of documentation', ' Codecademy Challenge'],
-    adjectives: [' a solid', ' a pointy', ' an infectious', ' a dangerous', ' a hard', ' a damned', ' an unexpected', ' a sharp', ' a powerful', ' a confusing'],
+    location: [' in the', ' at the', ' near the', ' right into the', ' below the', ' inside the', ' by the', ' through the', ' among the', ' above the', ' besides the', ' in front the'],
+    quantity: [' some', ' many', ' a few', ' a handful of'],
+    weapons: [' missiles', ' rockets', ' rays', ' spicebombs', ' bunch of code', ' virus in an e-mail', ' bugs', ' #FatalError', ' lot of documentation', ' Codecademy Challenges'],
+    adjectives: [' solid', ' pointy', ' infectious', ' dangerous', ' hard', ' damned', ' unexpected', ' sharp', ' powerful', ' confusing', ' enormous', ' massive'],
     adding: [' while', ' and', ' when'],
     end: '...',
     /*
@@ -63,6 +65,14 @@ const words = {
     get passive() {
         let len = this.verbs.passive.length-1;
         return this.verbs.passive[Math.round(Math.random()*len)];
+    },
+    get continue() {
+        let len = this.verbs.continuous.length-1;
+        return this.verbs.continuous[Math.round(Math.random()*len)];
+    },
+    get action() {
+        let len = this.verbs.actions.length-1;
+        return this.verbs.actions[Math.round(Math.random()*len)];
     },
     get locate() {
         let len = this.location.length-1;
@@ -87,3 +97,7 @@ const words = {
 };
 
 /* FIRST COMMIT DONE */
+/* SECOND COMMIT DONE
+    Adds continuous and action words
+    Adds .continue and .action
+*/
